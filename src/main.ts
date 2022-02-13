5b6d3c5e535c6455207a5fdfc0390757ad60ba75
maria <ma.usachova@gmail.com>
2022-02-14 00:37:53 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = new ConfigService().get('PORT') || 4242;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const port = process.env.PORT || 4242;
   await app.listen(port);
 }
 bootstrap();
