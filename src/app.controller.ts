7d2ad71285fd0774d1c5e567b83dfe08543feb2f
maria <ma.usachova@gmail.com>
2022-03-10 20:34:12 +0300

    hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index.hbs')
+  root() {
+    return { title: '😤🤟M33061😎😷️' };
+  }
+
+  @Get('ais')
+  @Render('ais.hbs')
+  ais() {
+    return { title: 'Архитектура информационных систем' };
+  }
+
+  @Get('web')
+  @Render('web.hbs')
+  web() {
+    return { title: 'Web-программирование' };
+  }
+
+  @Get('philosophy')
+  @Render('philosophy.hbs')
+  philosophy() {
+    return { title: 'Философия' };
+  }
+
+  @Get('practice')
+  @Render('practice.hbs')
+  practice() {
+    return { title: 'Практика' };
+  }
+
+  @Get('telecom')
+  @Render('telecom.hbs')
+  telecom() {
+    return { title: 'Телекоммуникационные системы и технологии' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', '/views/partials'));
 
   const port = process.env.PORT || 4242;
   await app.listen(port);
